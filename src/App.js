a05f9490a172929025a8ad5011227f61f6537ea4
lgitp9000 <ljphillips33@gmail.com>
2022-02-11 23:13:15 -0500

    Change color change what

--- src/App.js
+++ src/App.js
@@ -1,13 +1,13 @@
-import logo from './logo.svg';
+import luke from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        <img src={luke} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Luke Phillips 
         </p>
         <a
           className="App-link"
